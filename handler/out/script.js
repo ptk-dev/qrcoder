c2fefa83dba6a3d2376f0bbce68a76378c8c8cd3
Ptk <ptk.devid@gmail.com>
2024-04-28 12:44:33 +0530

    init

--- handler/out/script.js
+++ handler/out/script.js
@@ -1,13 +1,26 @@
-function convertRelativeToAbsolute() {
-    const baseUrl = "";
+const baseUrl = "https://raw.githubusercontent.com/ptk-dev/qr-coder-hosting-folder/main/website/";
+function convertRelativeToAbsolute(document) {
     const elementsWithRelativeUrls = document.querySelectorAll('[src], [href]');
 
     elementsWithRelativeUrls.forEach(element => {
         const attributeName = element.hasAttribute('src') ? 'src' : 'href';
         const relativeUrl = element.getAttribute(attributeName);
-        const absoluteUrl = new URL(relativeUrl, baseUrl).href;
+        const absoluteUrl = parseUrl(relativeUrl);
         element.setAttribute(attributeName, absoluteUrl);
     });
 }
 
-window.addEventListener('load', convertRelativeToAbsolute);
+function parseUrl(url) {
+    if (url === "/") url = "index.html"
+    let bn = new URL(url, "https://qr-coder.web.app").href.replace("https://qr-coder.web.app", baseUrl)
+    return bn
+}
+
+async function loadWebpage() {
+    const mainUrl = parseUrl(window.location.pathname)
+    const source = await (await fetch(mainUrl)).text()
+    const doc = new DOMParser().parseFromString(source, "text/html")
+    convertRelativeToAbsolute(doc)
+    document.documentElement.replaceWith(doc.documentElement)
+}
+window.addEventListener('load', loadWebpage);
